9e9b52ae4e5485aa4c26df0aa4c6ef4988d83acf
Jake Parolin <jakeparolin@Jakes-MacBook-Pro.local>
2018-05-29 13:52:18 -0700

    adjusting token for deployment

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "parolin"
+    let VERIFY_TOKEN = process.env.TOKEN
       
     // Parse the query params
     let mode = req.query['hub.mode'];
