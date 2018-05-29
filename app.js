f409e92a402b00d900cdecd35ced9198eb39a5fb
Jake Parolin <jakeparolin@Jakes-MacBook-Pro.local>
2018-05-29 15:13:34 -0700

    added handler functions and set up GET for for page-scoped ID

--- app.js
+++ app.js
@@ -4,24 +4,43 @@ const  app = express();
 
 app.use(bodyParser.json())
 
-app.post('/webhook', (req, res) => {
-    let body = req.body;
+// --Webhook--
 
-    if (body.object ==='page') {
-        body.entry.forEach(function(entry) {
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
-    res.status(200).send('EVENT_RECEIVED');
+app.post('/webhook', (req, res) => {  
+
+    // Parse the request body from the POST
+    let body = req.body;
+  
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page') {
+  
+      // Iterate over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Gets the body of the webhook event
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+        
+      });
+  
+      // Return a '200 OK' response to all events
+      res.status(200).send('EVENT_RECEIVED');
+  
     } else {
-        res.sendStatus(404);
+      // Return a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
     }
-})
+  
+  });
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
-    // Your verify token. Should be a random string.
+    // Takes token from Heroku.
     let VERIFY_TOKEN = process.env.TOKEN
       
     // Parse the query params
@@ -45,5 +64,22 @@ app.get('/webhook', (req, res) => {
       }
     }
   });
+
+  // --Handler Functions--
+
+  // Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
 
 module.exports = app
