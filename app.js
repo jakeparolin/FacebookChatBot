e7603e35a39dad15d2b1f3048ee3220f1e44188e
Jake Parolin <jakeparolin@Jakes-MacBook-Pro.local>
2018-05-29 13:10:55 -0700

    set up webhook

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express().use(bodyParser.json());
+const express = require('express');
+const bodyParser = require('body-parser');
+const  app = express();
 
-app.use(express.static('public'))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.json({message: 'API Example App'})
+app.post('/webhook', (req, res) => {
+    let body = req.body;
+
+    if (body.object ==='page') {
+        body.entry.forEach(function(entry) {
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+        });
+    res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
 })
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "parolin"
+      
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+  });
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 const app = require('./app')
 
-app.listen(3000, function () {
-    console.log('listening on port 3000');
-});
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
