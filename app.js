8eb2b999e5fe27300d47a8acd58d86ff59ac5f04
Jake Parolin <jakeparolin@Jakes-MacBook-Pro.local>
2018-05-30 11:39:18 -0700

    edit for token in POST method

--- app.js
+++ app.js
@@ -153,7 +153,7 @@ function callSendAPI(sender_psid, response) {
 
     // Send the HTTP request to the Messenger Platform
     request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages?access_token=<PAGE_ACCESS_TOKEN>",
+        "url": "https://graph.facebook.com/v2.6/me/messages?access_token=<PAGE_ACCESS_TOKEN>",
         "qs": { "access_token": PAGE_ACCESS_TOKEN },
         "method": "POST",
         "json": request_body
