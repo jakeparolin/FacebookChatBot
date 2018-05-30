7ec2cbfd093159fc3f34b21c4318f5a01d65de1e
Jake Parolin <jakeparolin@Jakes-MacBook-Pro.local>
2018-05-30 11:03:32 -0700

    fixed typo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const APP = require('./app')
 const PORT = process.env.PORT || 1337
 
-APP.listen(PORT, () => console.log('webhook is listening on port: ' + port));
+APP.listen(PORT, () => console.log('webhook is listening on port: ' + PORT));
